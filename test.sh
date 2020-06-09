10212403d942468d3e7913583a24bf7a85eb9b87
RhiannonPauling <rhiannonjane98@gmail.com>
2020-06-09 22:26:19 +1000

    Added test for inverse and root! Yay!

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-javac ./src/*.java -d classes
-java -cp classes javacalculator.JavaCalculator test
-=======
 javac ./src/*.java ./src/testingtool/tests/*.java ./src/testingtool/*.java -d classes
 java -cp classes javacalculator.JavaCalculator test
->>>>>>> master
+
