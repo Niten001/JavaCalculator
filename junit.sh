f70453c6ec9631029ad721fc13eac18e50b17999
Tim Martin <timartin1069@gmail.com>
2020-06-11 05:53:43 +1000

    Fixed issues with runniong junit on mac/linux

--- junit.sh
+++ junit.sh
@@ -1,2 +1,2 @@
-javac -cp junit/junit-4.13.jar;hamcreast-core-1.3.jar ./src/*.java ./src/calculator/*.java ./src/testingtool/tests/*.java ./src/testingtool/*.java ./src/unittests/*.java -d classes
-java -cp classes;junit/junit-4.13.jar;junit/hamcrest-core-1.3.jar org.junit.runner.JUnitCore UnitTests
+javac -cp junit/junit-4.13.jar:hamcrest-core-1.3.jar ./src/*.java ./src/calculator/*.java ./src/testingtool/tests/*.java ./src/testingtool/*.java ./src/unittests/*.java -d classes
+java -cp classes:junit/junit-4.13.jar:junit/hamcrest-core-1.3.jar org.junit.runner.JUnitCore UnitTests
